747007819dbfb30ded9dcdf5f6e98ce4a7b9802c
jry-king <2472559327@qq.com>
2020-09-10 23:19:30 +0800

    demo of a demo?

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
+import pt from './powertool.jpg';
 import './App.css';
+import { Button, Menu } from 'antd'; 
 
 function App() {
   return (
     <div className="App">
+      <Menu mode="horizontal">
+        <Menu.Item key="registration">
+          注册
+        </Menu.Item>
+        <Menu.Item key="profile">
+          个人资料
+        </Menu.Item>
+        <Menu.Item key="pay">
+          打赏
+        </Menu.Item>
+      </Menu>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={pt} className="App-logo" alt="logo" />
+        <p>博世电动工具员工打赏系统</p>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="primary">注册</Button>
+        <p></p>
+        <Button type="primary">登录</Button>
       </header>
     </div>
   );
